befefdb787258928a053ed0bd52f3c5899c47525
MahlatsiSekele <mahlatsi1234langa@gmail.com>
2024-08-27 09:47:03 +0200

    changes

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ document.querySelector('.slide-link').addEventListener('click', function(event) 
         window.location.href = this.href;
     }, 1000); // Delay to allow the transition to complete
 });
+// scripts.js
+
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
